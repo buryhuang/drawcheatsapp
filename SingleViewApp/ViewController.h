a0196d5e556d82149f8121cb94d04e41b2e6f41e
Haohua Huang <buryh@Haohuas-Mac-Pro.local>
2012-03-30 01:54:59 -0700

    Initial Commit

---
+++ SingleViewApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SingleViewApp
+//
+//  Created by Haohua Huang on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
