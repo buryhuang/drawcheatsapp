9eaaad79b91b3f76b937b1995368d25d02029b28
Haohua Huang <buryh@Haohuas-Mac-Pro.local>
2012-04-06 07:32:33 -0700

    Migrated project

---
+++ App/DictEnty.h
@@ -0,0 +1,13 @@
+//
+//  DictEnty.h
+//  Draw Cheats
+//
+//  Created by Haohua Huang on 4/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DictEnty : NSObject
+
+@end
