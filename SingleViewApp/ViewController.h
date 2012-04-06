fab9fe6eb4f6f8f383021168053b3479f8fcc155
Haohua Huang <buryh@Haohuas-Mac-Pro.local>
2012-04-06 04:44:25 -0700

    Version 0.01

--- SingleViewApp/ViewController.h
+++ SingleViewApp/ViewController.h
@@ -8,6 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
+@class UIDictTableView;
+
 @interface ViewController : UIViewController
+    <UITextFieldDelegate> {
+    
+    UITextField * mLetters;
+    
+    UILabel *mLengthLabel;
+    UISegmentedControl *mLength;
+    
+    UILabel *mNoResultLabel;
+    
+    UIDictTableView * mContentTable;
+
+}
+
+@property (nonatomic,retain) IBOutlet UITextField * letters;
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;
+
+@property (nonatomic,retain) IBOutlet UILabel *lengthLabel;
+@property (nonatomic,retain) IBOutlet UISegmentedControl *length;
+
+@property (nonatomic,retain) IBOutlet UILabel *noResultLabel;
+
+@property (nonatomic, retain) IBOutlet UIDictTableView *contentTable;
+
+-(IBAction) segmentedControlIndexChanged;
+
+- (void)updateResult;
 
 @end
